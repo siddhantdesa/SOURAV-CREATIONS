d080243f5141f61042d6636670e1edd01781e056
siddhant desai <agojuster@gmail.com>
2026-09-04 01:40:34 +0530

    Delete src directory

--- src/services/config.js
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-  USE_MOCK: true,
-  API_URL: import.meta.env.VITE_API_URL || 'https://api.souravcreations.com/v1',
-  SIMULATED_DELAY: 350
-};
-
-export const sleep = (ms = CONFIG.SIMULATED_DELAY) =>
-  new Promise((resolve) => setTimeout(resolve, ms));
